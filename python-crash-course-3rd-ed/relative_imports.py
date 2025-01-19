69d7b0ea01258dddc1196c9fc845eea6b7999a3f
Vidhya Chidambaram <vchidamb@redhat.com>
2025-01-19 15:55:36 -0500

    another classes

--- python-crash-course-3rd-ed/relative_imports.py
+++ python-crash-course-3rd-ed/relative_imports.py
@@ -1,4 +1,12 @@
 from tryit.try9192 import *
+from cars.cars import Car
+
+def print_car_info():
+    car = Car("Yellow", "Lamborghini")
+    car.print_descriptive_name()
 
 if __name__ == "__main__":
+    print("----")
     three_restaurants()
+    print("-----")
+    print_car_info()
